71cb4166d1c918777a0a885398aa95e00d41d2d9
jorge borga <l18390063@chetumal.tecnm.mx>
2022-11-15 17:17:31 -0500

    crud hecho

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
         $viewData["title"] = "About us - Online Store";
         $viewData["subtitle"] = "About us";
         $viewData["description"] = "This is an about page ...";
-        $viewData["author"] = "Developed by: Your Name";
+        $viewData["author"] = "Developed by: Jorge Borga";
         return view('home.about')->with("viewData", $viewData);
     }
 }
